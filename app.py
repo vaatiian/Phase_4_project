113424a0341415c529568068a2f76f56a3580fe4
ianvaati <vaatiian1@gmail.com>
2023-12-13 21:04:16 +0300

    updated code

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+import matplotlib.pyplot as plt
+
+# Load the serialized ARIMA model
+def load_arima_model():
+    # Load your serialized ARIMA model here
+    with open('arima_model.pkl', 'rb') as model_file:
+        arima_model = pickle.load(model_file)
+    return arima_model
+
+# Function to make predictions using the ARIMA model
+def make_predictions(model, data):
+    # Perform predictions using the ARIMA model
+    predictions = model.predict(n_periods=len(data))
+    return predictions
+
+# Function to visualize stock price data
+def visualize_data(actual_data, predicted_data):
+    # Visualize historical and predicted data using matplotlib or other libraries
+    plt.figure(figsize=(10, 6))
+    plt.plot(actual_data, label='Actual Data')
+    plt.plot(predicted_data, label='Predicted Data', color='red')
+    plt.title('Stock Price Prediction')
+    plt.xlabel('Time Steps')
+    plt.ylabel('Stock Price')
+    plt.legend()
+    plt.show()
+
+def main():
+    st.title('Stock Price Prediction with ARIMA Model')
+
+    # Load the serialized ARIMA model
+    arima_model = load_arima_model()
+
+    # Data input section
+    st.header('Enter Historical Stock Prices:')
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+    if uploaded_file is not None:
+        # Read the uploaded file
+        df = pd.read_csv(uploaded_file)
+        
+        # Perform predictions
+        predictions = make_predictions(arima_model, df)
+        
+        # Visualization
+        st.subheader('Stock Price Prediction:')
+        visualize_data(df, predictions)
+        
+        # Show metrics or additional details
+        st.subheader('Model Performance Metrics:')
+        # Display relevant metrics (e.g., RMSE, MAE)
+        st.write("Placeholder for model metrics")
+    
+    else:
+        st.write('Upload a CSV file to get started.')
+
+if __name__ == '__main__':
+    main()
